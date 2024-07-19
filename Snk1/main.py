bcde0cd3bc518bf57c3a3304c93305905adc1248
winiyaz <winiyaz@proton.me>
2024-07-19 11:07:49 -0700

    -SetUpSnake

--- Snk1/main.py
+++ Snk1/main.py
@@ -7,6 +7,21 @@ scn.setup(width=600, height=600)
 scn.bgcolor("#020617")
 scn.title("PussyJuices")
 
+sta_pos = [(0, 0), (-20, 0), (-40, 0)]  # Coordinate positions
+segs = []
+
+# Creating the diagram
+for pos in sta_pos:
+	new_seg = Turtle("square")
+	new_seg.color("#db2777")
+	new_seg.penup()
+	new_seg.goto(pos)
+	segs.append(new_seg)
+
+game_is_on = True
+while game_is_on:
+	for s in segs:
+		s.forward(20)
 
 
 
