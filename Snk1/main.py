19cee5de90e1c3b71e0bda8cf583d65cac69351f
winiyaz <winiyaz@proton.me>
2024-07-19 10:54:29 -0700

    -ReadmePasd

---
+++ Snk1/main.py
@@ -0,0 +1,18 @@
+# D20 -
+
+from turtle import Turtle, Screen
+
+scn = Screen()
+scn.setup(width=600, height=600)
+scn.bgcolor("#020617")
+scn.title("PussyJuices")
+
+
+
+
+
+
+
+# ---
+scn.exitonclick() # Exit on clic
+# ---
