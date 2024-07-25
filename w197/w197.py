1366e2117d415ebef3311def4b70097a37fa1bb6
winiyaz <winiyaz@proton.me>
2024-07-25 10:37:42 -0700

    -197Slicer

---
+++ w197/w197.py
@@ -0,0 +1,14 @@
+# Lesson 197 on slicey
+
+piano_keys = ["a", "b", "c", "d", "e", "f"]
+# positios     0    1    2    3    4    5
+# Remember these positions
+
+print(f'''
+Slicer
+---
+{piano_keys[1:4]}
+''')
+
+
+
