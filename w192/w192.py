b0e0e6c05be3cec94228a6a700a70d34b4d23a0b
winiyaz <winiyaz@proton.me>
2024-07-25 02:52:39 -0700

    -AddPretification

--- w192/w192.py
+++ w192/w192.py
@@ -1,2 +1,25 @@
 # Work for vid 192
 
+class Animal:
+	def __init__(self):
+		self.num_eyes = 2
+
+	def breathe(self):
+		print("Inhale, Exhale")
+
+class Fish(Animal):
+	def __init__(self):
+		super().__init__()
+
+	def breathe(self):
+		super().breathe()
+		print('Doing Under water')
+
+	def swim(self):
+		print('Moving in water')
+
+
+nemo = Fish()
+# nemo.swim()
+nemo.breathe()
+print(nemo.num_eyes)
